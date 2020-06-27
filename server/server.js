825112b3fe651286f5d348b2ca8b2bbf177476ce
angelrepublic24 <angelrepublic24@gmail.com>
2020-06-26 21:32:08 -0400

    First commit

---
+++ server/server.js
@@ -0,0 +1,54 @@
+require('../config/config')
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser')
+
+
+// Parse application /x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application json
+app.use(bodyParser.json())
+
+
+
+
+
+app.get('/usuario', (req, res) => {
+    res.json('get usuario');
+})
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    if (body.name == undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        })
+    } else {
+        res.json({
+            usuario: body
+        });
+    }
+
+
+})
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+})
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete usuario');
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando el puerto ${process.env.PORT}`);
+});
